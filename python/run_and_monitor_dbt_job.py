73cd979cf2b20285ef90b41f9f2a440aaeeb1f2b
Diego Alejandro Robledo <68504016+alexriley7@users.noreply.github.com>
2023-08-17 18:38:33 -0300

    Update run_and_monitor_dbt_job.py

--- python/run_and_monitor_dbt_job.py
+++ python/run_and_monitor_dbt_job.py
@@ -5,20 +5,16 @@ import time
 import json
 
 #------------------------------------------------------------------------------
-# get environment variables 
+# get environment variables
 #------------------------------------------------------------------------------
 api_base        = os.getenv('DBT_URL', 'https://cloud.getdbt.com/') # default to multitenant url
 job_cause       = os.getenv('DBT_JOB_CAUSE', 'API-triggered job') # default to generic message
 git_branch      = os.getenv('DBT_JOB_BRANCH', None) # default to None
 schema_override = os.getenv('DBT_JOB_SCHEMA_OVERRIDE', None) # default to None
-api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided<<<<<<< HEAD
-account_id      = os.environ[190628] # no default here, just throw an error here if id not provided
-project_id      = os.environ[278741] # no default here, just throw an error here if id not provided
-job_id          = os.environ[400454] # no default here, just throw an error here if id not provided
-
+api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided
 account_id      = os.environ['DBT_ACCOUNT_ID'] # no default here, just throw an error here if id not provided
 project_id      = os.environ['DBT_PROJECT_ID'] # no default here, just throw an error here if id not provided
-job_id          = os.environ['DBT_PR_JOB_ID'] # no default here, just throw an error here if id not provided>>>>>>> 37e14872d0df4978e800ca6dc0a50867f21e2731
+job_id          = os.environ['DBT_PR_JOB_ID'] # no default here, just throw an error here if id not provided
 
 print(f"""
 Configuration:
