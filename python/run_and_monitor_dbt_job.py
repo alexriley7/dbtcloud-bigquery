2e043e901451b50b4a5df20a9117c6c61a6f7014
Diego Alejandro Robledo <68504016+alexriley7@users.noreply.github.com>
2023-08-17 18:16:22 -0300

    Update run_and_monitor_dbt_job.py

--- python/run_and_monitor_dbt_job.py
+++ python/run_and_monitor_dbt_job.py
@@ -12,9 +12,9 @@ job_cause       = os.getenv('DBT_JOB_CAUSE', 'API-triggered job') # default to g
 git_branch      = os.getenv('DBT_JOB_BRANCH', None) # default to None
 schema_override = os.getenv('DBT_JOB_SCHEMA_OVERRIDE', None) # default to None
 api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided
-account_id      = os.environ['190628'] # no default here, just throw an error here if id not provided
-project_id      = os.environ['278741'] # no default here, just throw an error here if id not provided
-job_id          = os.environ['400454'] # no default here, just throw an error here if id not provided
+account_id      = os.environ[190628] # no default here, just throw an error here if id not provided
+project_id      = os.environ[278741] # no default here, just throw an error here if id not provided
+job_id          = os.environ[400454] # no default here, just throw an error here if id not provided
 
 print(f"""
 Configuration:
