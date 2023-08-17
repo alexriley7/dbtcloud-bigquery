37e14872d0df4978e800ca6dc0a50867f21e2731
Diego Alejandro Robledo <68504016+alexriley7@users.noreply.github.com>
2023-08-17 18:23:50 -0300

    Update run_and_monitor_dbt_job.py

--- python/run_and_monitor_dbt_job.py
+++ python/run_and_monitor_dbt_job.py
@@ -14,7 +14,7 @@ schema_override = os.getenv('DBT_JOB_SCHEMA_OVERRIDE', None) # default to None
 api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided
 account_id      = os.environ['DBT_ACCOUNT_ID'] # no default here, just throw an error here if id not provided
 project_id      = os.environ['DBT_PROJECT_ID'] # no default here, just throw an error here if id not provided
-job_id          = os.environ['DBT_JOB_ID'] # no default here, just throw an error here if id not provided
+job_id          = os.environ['DBT_PR_JOB_ID'] # no default here, just throw an error here if id not provided
 
 print(f"""
 Configuration:
